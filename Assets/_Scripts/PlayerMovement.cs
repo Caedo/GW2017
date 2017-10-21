d1cb5b3bf4bbda1ff6735ca3974231e7cf17318a
Stefan <dyjakstefan@gmail.com>
2017-10-21 16:23:09 +0200

    no message

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jump;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveY = jump;
+        bool isJump = Input.GetButton("Jump");
+        
+        if(isJump)
+        {
+            moveY = jump;
+        }
+        else
+        {
+            moveY = 0.0f;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, moveY, moveVertical);
+
+
+        rb.AddForce(movement * speed);
+    }
+
+}
