2b47f4a53935621b8a19d54a600364014d6ce313
Paweł <dziedzicpm@gmail.com>
2017-10-21 14:51:54 +0200

    Menu States

---
+++ Assets/_Scripts/MenuStateMachine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MenuStateMachine : MonoBehaviour {
+
+    public List<MenuState> m_MenuStates = new List<MenuState>();
+
+    Stack<MenuState> m_StateStack = new Stack<MenuState>();
+
+    public void PushState<T>() where T : MenuState {
+        MenuState state = m_MenuStates.FirstOrDefault(s => s is T);
+        state = Instantiate(state, transform, false);
+
+        m_StateStack.Push(state);
+    }
+
+    public void PopState() {
+        MenuState state = m_StateStack.Pop();
+        Destroy(state.gameObject);
+    }
+}
