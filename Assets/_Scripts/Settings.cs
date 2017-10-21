ae8eddfaa2635291b97595a3e331fee178c62657
Stefan <dyjakstefan@gmail.com>
2017-10-21 20:51:03 +0200

    Settings

---
+++ Assets/_Scripts/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MenuState {
+
+    public void VolumeChange(float value)
+    {
+        PlayerPrefs.SetFloat("Volume", value);
+    }
+
+    public void MusicChange(float value)
+    {
+        PlayerPrefs.SetFloat("Music", value);
+    }
+
+    public void SFXChange(float value)
+    {
+        PlayerPrefs.SetFloat("SFX", value);
+    }
+}
