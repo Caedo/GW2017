002f377d0a7cab181e98d1e73388029af9cdc4b6
Paweł <dziedzicpm@gmail.com>
2017-10-21 23:54:49 +0200

    Animations

---
+++ Assets/_Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    Animator animator;
+
+    PlayerMovement playerMovement;
+    PlayerItemsController playerItemsController;
+
+    private void Awake() {
+        animator = GetComponentInChildren<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerItemsController = GetComponent<PlayerItemsController>();
+
+        playerItemsController.OnItemThrow += OnItemThrow;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        animator.SetFloat("speedPercent", playerMovement.SpeedPercent, .1f, Time.deltaTime);
+    }
+
+    void OnItemThrow(Item item) {
+        animator.SetTrigger("throw");
+    }
+}
