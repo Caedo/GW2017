95463dc88bc2192437bc44ab579cacc728fdc1ef
Papierz <ppp16@vp.pl>
2017-10-21 18:25:44 +0200

    Change to Polish lang

---
+++ Assets/_Scripts/Menu/CreditsController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MenuState {
+
+	public void Back(){
+		MenuStateMachine.Instance.PushState<TitleScreenController> ();
+	}
+}
