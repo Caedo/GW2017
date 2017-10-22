0b08889c87805ef8b1a115ebc70d426afa3e3af7
Stefan <dyjakstefan@gmail.com>
2017-10-22 11:35:58 +0200

    sd

--- Assets/_Scripts/Respawn.cs
+++ Assets/_Scripts/Respawn.cs
@@ -14,6 +14,9 @@ public class Respawn : MonoBehaviour {
 
 		yield return new WaitForSeconds (3);
 		Player.enabled = !Player.enabled;
+        Rigidbody rb = Player.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
 		Player.gameObject.transform.position = new Vector3 (30f, 10f, 30f);
-	}
+        rb.isKinematic = false;
+    }
 }
