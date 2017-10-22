3762227b2d6939f77a1a8238b2e9445409f7f5c5
Paweł <dziedzicpm@gmail.com>
2017-10-22 06:36:05 +0200

    movement fix

---
+++ Assets/NewCharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NewCharacterMovement : MonoBehaviour {
+
+    public float speedMultiplier;
+    new Rigidbody rigidbody;
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        float horizontal = Input.GetAxis("Player1horizontal");
+        float vertical = Input.GetAxis("Player1vertical");
+
+        Vector3 newVelocity = new Vector3(horizontal, 0, vertical).normalized * speedMultiplier;
+
+        rigidbody.MovePosition(rigidbody.position + newVelocity * Time.fixedDeltaTime);
+    }
+}
